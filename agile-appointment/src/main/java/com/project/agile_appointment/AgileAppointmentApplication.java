0bf0bd39c529d3d3366274a170e073a6e9563cc1
mylenaverspeelt <mylena49@hotmail.com>
2024-06-20 13:51:04 -0300

    1st commit / config spring and h2 database

---
+++ agile-appointment/src/main/java/com/project/agile_appointment/AgileAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.project.agile_appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileAppointmentApplication.class, args);
+	}
+
+}
