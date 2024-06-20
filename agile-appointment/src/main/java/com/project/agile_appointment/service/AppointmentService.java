27c8299be717335d465e0505d994894ac5b18ffc
mylenaverspeelt <mylena49@hotmail.com>
2024-06-20 15:24:04 -0300

    create appointment service

--- agile-appointment/src/main/java/com/project/agile_appointment/service/AppointmentService.java
+++ agile-appointment/src/main/java/com/project/agile_appointment/service/AppointmentService.java
@@ -1,12 +1,41 @@
 package com.project.agile_appointment.service;
 
 import com.project.agile_appointment.models.Appointment;
+import com.project.agile_appointment.models.Patient;
 import com.project.agile_appointment.repository.AppointmentRepository;
 import com.project.agile_appointment.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 public class AppointmentService {
+
+    private final AppointmentRepository appointmentRepository;
+    private final PatientRepository patientRepository;
+
+    @Autowired
+    public AppointmentService(AppointmentRepository appointmentRepository, PatientRepository patientRepository) {
+        this.appointmentRepository = appointmentRepository;
+        this.patientRepository = patientRepository;
+    }
+
+    public Appointment scheduleAppointment(Long patientId, LocalDateTime dateAndTime, String specialty) throws Exception {
+
+        Optional<Patient> registeredPatient = patientRepository.findById(patientId);
+        if (registeredPatient.isEmpty()) {
+            throw new Exception("Paciente não encontrado.");
+        }
+
+        Patient patient = registeredPatient.get();
+        Appointment appointment = new Appointment();
+        appointment.setPatient(patient);
+        appointment.setDateAndTime(dateAndTime);
+        appointment.setSpecialty(specialty);
+
+        return appointmentRepository.save(appointment);
+    }
 
 }
